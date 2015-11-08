681f2f4a5c6da1db6728c16696cadfe1496521a0
Lee <leealanberg@gmail.com>
2015-11-08 01:11:11 -0600

    Updated MMS_StopLab.ps1 in Azure Automation.

--- Runbooks/MMS_StopLab.ps1
+++ Runbooks/MMS_StopLab.ps1
@@ -1,13 +1,23 @@
 #
 #
 #
+New-EventLog -Logname System -Source OMSAutomation
+
+Write-EventLog -EventId 1 -LogName System -Message "Runbook MMS_StopLab Started" -Source OMSAutomation
 
 $LabMachines = "Nano-01","Nano-02","Nano-03","Nano-04"
 	
 ForEach($Server in $LabMachines)
 {
 	get-vm $Server -ErrorAction SilentlyContinue | stop-vm -TurnOff -Force -Passthru	
+	
+	Write-EventLog -EventId 2 -LogName System -Message "Runbook MMS_StopLab Stopped Server $Server" -Source OMSAutomation
+	
+	
 }
+
+
+Write-EventLog -EventId 3 -LogName System -Message "Runbook MMS_StopLab Complete" -Source OMSAutomation
 
 
 #
