bf468e11d928af8e173bba7abaabc8d34af818dd
Lee <leealanberg@gmail.com>
2015-11-08 01:29:24 -0600

    Updated MMS_DeployNano.ps1 in Azure Automation.

--- Runbooks/MMS_DeployNano.ps1
+++ Runbooks/MMS_DeployNano.ps1
@@ -15,23 +15,31 @@ param($VMName, $VHDFile, $VMMemory, $VMDiskSize )
 
 }
 
+New-EventLog -Logname System -Source OMSAutomation
+
+Write-EventLog -EventId 1 -LogName System -Message "Runbook MMS_DeployNano Started" -Source OMSAutomation
+
 Write-Output "Staring to Create Nano Server!" 
 
 #Run the Actions!
 Write-Output "Staring to Create Nano Server 1!" 
+
 CreateNanoServer -VMName "Nano-01" -VHDFile "Nano-01.vhd" -VMMemory 512MB -VMDiskSize 5GB
+Write-EventLog -EventId 2 -LogName System -Message "Runbook MMS_DeployNano Creating Server Nano-01" -Source OMSAutomation
 
 
 Write-Output "Staring to Create Nano Server 2" 
 CreateNanoServer -VMName "Nano-02" -VHDFile "Nano-02.vhd" -VMMemory 512MB -VMDiskSize 5GB
+Write-EventLog -EventId 2 -LogName System -Message "Runbook MMS_DeployNano Creating Server Nano-02" -Source OMSAutomation
 
 
 Write-Output "Staring to Create Nano Server 3!" 
 CreateNanoServer -VMName "Nano-03" -VHDFile "Nano-03.vhd" -VMMemory 512MB -VMDiskSize 5GB
+Write-EventLog -EventId 2 -LogName System -Message "Runbook MMS_DeployNano Creating Server Nano-03" -Source OMSAutomation
 
 
 Write-Output "Staring to Create Nano Server 4!" 
 CreateNanoServer -VMName "Nano-04" -VHDFile "Nano-04.vhd" -VMMemory 512MB -VMDiskSize 5GB
-
+Write-EventLog -EventId 2 -LogName System -Message "Runbook MMS_DeployNano Creating Server Nano-04" -Source OMSAutomation
 
 Write-Output "Done!" 
